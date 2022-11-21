3331b2e01fce573b7f46a6b07de2abd082c27ee5
rowlandseymour <rowlandseymour@gmail.com>
2022-11-21 19:18:28 +0000

    add new chapters

---
+++ lab solutions/lab2_ex_1.R
@@ -0,0 +1,22 @@
+multivariate.normal.density <- function(x, mu, sigma){
+
+  #Evaluate preliminary values
+  k <- dim(sigma)[1]
+  sigma.inv <- solve(sigma)
+  sigma.det <- det(sigma)
+
+  #Evalue pdf
+  sq.root.terms <- (2*pi)^(-k/2)*sigma.det^(-k/2)
+  exponent.term <- -0.5*t(x-mu)%*%solve(sigma)%*%(x-mu)
+
+  #Return pdf
+  result <- sq.root.terms*exp(exponent.term)
+  return(result)
+
+}
+
+
+multivariate.normal.density(c(0, 1, 2), c(1, 1, 1), diag(3))
+
+
+
